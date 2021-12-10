99b317c80222ffddabd604f15272d6f687a95393
Ayush Gupta <ayushgupta197@gmail.com>
2021-12-11 00:50:42 +0530

    :arrow_right_hook: add useSavedTracks hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,11 @@
 import useNowPlayingTrack from './useNowPlayingTrack';
 import useRecentlyPlayedTracks from './useRecentlyPlayedTracks';
+import useTopTracksOrArtists from './useTopTracksOrArtists';
 import useUserPlaylists from './useUserPlaylists';
 
-export { useNowPlayingTrack, useRecentlyPlayedTracks, useUserPlaylists };
+export {
+  useNowPlayingTrack,
+  useRecentlyPlayedTracks,
+  useTopTracksOrArtists,
+  useUserPlaylists,
+};
--- src/utils/fetchCurrentUsersSavedTracks.ts
+++ src/utils/fetchCurrentUsersSavedTracks.ts
@@ -5,7 +5,7 @@ import { CURRENT_USER_SAVED_TRACKS_URL } from '../constants';
 /**
  * Get a list of the songs saved in the current Spotify user’s ‘Your Music’ library.
  *
- * @see {@link https://developer.spotify.com/documentation/web-api/reference/#/operations/get-users-saved-trackss}
+ * @see {@link https://developer.spotify.com/documentation/web-api/reference/#/operations/get-users-saved-tracks}
  * @param {number} [limit] - The maximum number of playlists to return. Default: 20. Minimum: 1. Maximum: 50.
  * @return {Promise}  contains an array of saved track objects (wrapped in a paging object) in JSON format.
  */
